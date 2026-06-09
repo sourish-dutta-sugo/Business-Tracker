49dc1f476b695fb3355113ccf52717c4c9ed8d27
SOURISH DUTTA (Sugo) <sourishdutta2007@gmail.com>
2026-06-09 18:23:48 +0530

    Simplify FY storage and rebuilt Invoice

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -3,6 +3,7 @@ package com.example
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onRoot
 import com.example.ui.theme.MyApplicationTheme
+import com.vibecoding.zerobook_androidonly0.Greeting
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
